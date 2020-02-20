df28f4dd6475fb2c4fe1975e07ab6fa625b375c6
Hivecluster901 <bill981212@gmail.com>
2020-02-20 20:52:24 +0900

    Within the middle of chili's beginners tutorial 9

---
+++ Engine/Poo.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Poo
+{
+public:
+    void Update();
+    int x;
+    int y;
+    int vx;
+    int vy;
+    int width = 24;
+    int height = 24;
+    bool isEaten = false;
+};
