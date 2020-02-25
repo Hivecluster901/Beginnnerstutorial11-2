17cd700f5a59839fce613758aad334caf62d5f4e
Hivecluster901 <bill981212@gmail.com>
2020-02-25 16:37:28 +0900

    The end of tutorial 11

--- Engine/Poo.h
+++ Engine/Poo.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include "Graphics.h"
+#include "Dude.h"
 class Poo
 {
 public:
     void Update();
-    void Processconsumption(int dudex, int dudey, int dudewidth, int dudeheight);
+    void Processconsumption(const Dude& dude);//In general, we should pass an object by reference. This is because copying an object to a parameter will take a lot of time and make our program slow.
+                                              // const keyword says we are not changing any value of dude object.(Read-Only)
+    void Draw(Graphics& gfx) const; // We can only pass gfx by reference. When gfx is passed as an argument, it should be copied to the parameter but this is impossible.
+    
+    //Draw is not a mutator. Thus, we need to put const keyword at the end. 
     int x;
     int y;
     int vx;
