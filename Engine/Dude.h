97baa66f7abcdcc8f67f6d4456e301e6bef06bea
Hivecluster901 <bill981212@gmail.com>
2020-02-25 17:15:15 +0900

    Mysol

--- Engine/Dude.h
+++ Engine/Dude.h
@@ -1,10 +1,11 @@
 #pragma once
-
+#include "Graphics.h"
 
 class Dude
 {
 public:
 	void ClampToScreen();
+	void Draw(Graphics& gfx);
 	int x = 400;
 	int y = 300;
 	static constexpr int width = 20;
