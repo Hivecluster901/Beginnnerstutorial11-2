2d38290d933d7ad1819b334ea674ae524f46b8c5
Hivecluster901 <bill981212@gmail.com>
2020-02-25 17:22:43 +0900

    Chili's sol

--- Engine/Dude.h
+++ Engine/Dude.h
@@ -1,10 +1,11 @@
 #pragma once
-
+#include "Graphics.h"
 
 class Dude
 {
 public:
 	void ClampToScreen();
+	void Draw(Graphics& gfx) const;
 	int x = 400;
 	int y = 300;
 	static constexpr int width = 20;
